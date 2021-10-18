20dd0ee4d4adef8b905fbfaf504a63bdc8781793
sdarbucks <kdh797979@naver.com>
2021-10-18 12:40:30 +0900

    update

--- poll/urls.py
+++ poll/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('<int:question_id>/result/', views.result, name='result'),
-
 ]
